2dd141286a98b6c1b6f573aaeba625c5fa916f97
Jasmine <jayjadesanders@gmail.com>
2025-03-22 14:44:31 -0400

    Added functional local storage to post

--- main.js
+++ main.js
@@ -10,4 +10,34 @@ if (!localStorage.getItem("blogPosts")){
 }
 
 console.log(JSON.parse(localStorage.getItem("blogPosts")))
+const blogContainer = document.getElementById("blogContainer")
 
+function postLocalStorage(){
+    let blogPosts = JSON.parse(localStorage.getItem("blogPosts"))
+    for (const [title, post] of Object.entries(blogPosts)){
+        console.log(post)
+        addPostElement(title, post)
+    }
+}
+
+function addPostElement(title, post){
+    const newLi = document.createElement("li")
+    const newH1 = document.createElement("h2")
+    const newP = document.createElement("p")
+
+    const titleContent = document.createTextNode(title)
+    const postContent = document.createTextNode(post)
+    
+    newH1.appendChild(titleContent)
+    newP.appendChild(postContent)
+    newLi.appendChild(newH1)
+    newLi.appendChild(newP)
+
+    newLi.classList.add("blog-post")
+    newH1.classList.add("title")
+    newP.classList.add("post")
+
+    blogContainer.appendChild(newLi)
+}
+
+postLocalStorage()
