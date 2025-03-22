7ea2040fe64ecb64af23c8b106e5225290a04f86
Jasmine <jayjadesanders@gmail.com>
2025-03-22 16:46:36 -0400

    added submit to local storage functionality

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ if (document.URL.includes("index.html")){
 const blogContainer = document.getElementById("blogContainer")
 
 let temp = {
-    "Post One": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
-    "Post Two": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
-    "Post Three": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
+    // "Post One": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
+    // "Post Two": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
+    // "Post Three": "Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis quis explicabo tenetur veniam voluptate sequi sit inventore beatae quam.",
 }
 
 if (!localStorage.getItem("blogPosts")){
@@ -50,6 +50,8 @@ postLocalStorage()
 // Form
 
 if (document.URL.includes("new-post.html")) {
+
+
     const postError = document.getElementById("postError")
     const titleError = document.getElementById("titleError")
 
@@ -66,6 +68,7 @@ if (document.URL.includes("new-post.html")) {
         postvalidated = validatePost(newPost)
 
         if(titlevalidated && postvalidated){
+            post(newTitle, newPost)
             console.log("post")
         }
     }
@@ -92,4 +95,16 @@ if (document.URL.includes("new-post.html")) {
         }
     }
 
+    function post(title, post) {
+        if (!localStorage.getItem("blogPosts")){
+            let firstPost = {}
+            firstPost[title] = post
+            localStorage.setItem("blogPosts", JSON.stringify(firstPost))
+        }else {
+            let blogPosts = JSON.parse(localStorage.getItem("blogPosts"))
+            console.log(blogPosts)
+            blogPosts[title] = post
+            localStorage.setItem("blogPosts", JSON.stringify(blogPosts))
+        }
+    }
 }
